daf3407cc625fc9dfe82efe637c7baf0d39caad2
Arseniy Krasnov <arseniy@krasnoff.org>
2015-03-01 00:37:50 +0300

    some refactoring

--- auth/provider/instant.php
+++ auth/provider/instant.php
@@ -2,24 +2,16 @@
 
 namespace arseniyk\instant\auth\provider;
 
-class instant extends \phpbb\auth\provider\base
+class instant extends \phpbb\auth\provider\base {
 
-{
-
-    public function __construct(\phpbb\db\driver\driver_interface $db)
-    {
+    public function __construct(\phpbb\db\driver\driver_interface $db) {
         $this->db = $db;
     }
 
-    public function login($username, $password)
-    {
-        // Auth plugins get the password untrimmed.
-        // For compatibility we trim() here.
+    public function login($username, $password) {
+		error_log(print_r('login', TRUE)); 
         $password = trim($password);
-
-        // do not allow empty password
-        if (!$password)
-        {
+        if (!$password) {
             return array(
                 'status'    => LOGIN_ERROR_PASSWORD,
                 'error_msg'    => 'NO_PASSWORD_SUPPLIED',
@@ -27,47 +19,103 @@ class instant extends \phpbb\auth\provider\base
             );
         }
 
-        if (!$username)
-        {
+        if (!$username) {
             return array(
                 'status'    => LOGIN_ERROR_USERNAME,
                 'error_msg'    => 'LOGIN_ERROR_USERNAME',
                 'user_row'    => array('user_id' => ANONYMOUS),
             );
-        }
-
-        $username_clean = utf8_clean_string($username);
-		$logged_id  = cmsUser::login($email, $password, $remember);
+        }       		
 		
-		 $sql = 'SELECT user_id, username, user_password, user_passchg, user_pass_convert, user_email, user_type, user_login_attempts
-            FROM ' . USERS_TABLE . "
-            WHERE username_clean = '" . $this->db->sql_escape($username_clean) . "'";
-        $result = $this->db->sql_query($sql);
-        $row = $this->db->sql_fetchrow($result);
-        $this->db->sql_freeresult($result);
+		$sql = sprintf('SELECT nickname, email, password, password_salt FROM cms_users WHERE nickname = \'%1$s\'', $this->db->sql_escape($username));
+		$iresult = $this->db->sql_query($sql);
+		$irow = $this->db->sql_fetchrow($iresult);
+		$this->db->sql_freeresult($iresult);
+		if ($irow) {
 		
-		if ( $logged_id ){
-			// Successful login... set user_login_attempts to zero...
-			if ($row) {
+			if ($irow["password"] == MD5(MD5($password) . $irow["password_salt"])) {
+				$sql = sprintf('SELECT user_id, username, user_password, user_passchg, user_email, user_type FROM %1$s WHERE username = \'%2$s\'', USERS_TABLE, $this->db->sql_escape($username));
+				$result = $this->db->sql_query($sql);
+				$row = $this->db->sql_fetchrow($result);
+				$this->db->sql_freeresult($result);
+				if ($row){
+				   return array(
+						'status'		=> LOGIN_SUCCESS,
+						'error_msg'		=> false,
+						'user_row'		=> $row,
+					);
+				}
+				
 				return array(
-					'status'        => LOGIN_SUCCESS,
-					'error_msg'        => false,
-					'user_row'        => $row,
-				);
-			}
-			return array(
-					'status'        => LOGIN_SUCCESS_CREATE_PROFILE,
-					'error_msg'        => false,
-					'user_row'     => array(
-                           "username"       => \cmsUser::get('nickname'),  // Отображаемое имя пользователя
-                           "user_password"  => '',  // phpbb-хеш пароля
-                           "user_email"     => $username_clean,  // E-mail пользователя, если существует
-                           "user_type"      => 0,
-                           "group_id" => 2
-                       ),
+					'status'		=> LOGIN_SUCCESS_CREATE_PROFILE,
+					'error_msg'		=> false,
+					'user_row'		=> $this->newUserRow($irow["nickname"], $irow["email"], $irow["password"]),
 				);
 			
+			}
+			
+			return array(
+                'status'    => LOGIN_ERROR_PASSWORD,
+                'error_msg'    => 'LOGIN_ERROR_PASSWORD',
+                'user_row'    => array('user_id' => ANONYMOUS),
+            );
 		}
     }
+	
+	public function autologin()
+	{	
+		global $request;
+		$auth_token = $request->variable(array("icms", "auth"), '', false, \phpbb\request\request_interface::COOKIE);
+		$sql = sprintf('SELECT nickname, email, password FROM cms_users WHERE auth_token = \'%1$s\'', $this->db->sql_escape($auth_token));
+		$iresult = $this->db->sql_query($sql);
+		$irow = $this->db->sql_fetchrow($iresult);
+		$this->db->sql_freeresult($iresult);
+		if ($irow) {
+			$sql = sprintf('SELECT user_id, username, user_password, user_passchg, user_email, user_type FROM %1$s WHERE username = \'%2$s\'', USERS_TABLE, $this->db->sql_escape($irow["nickname"]));
+			$result = $this->db->sql_query($sql);
+			$row = $this->db->sql_fetchrow($result);
+			$this->db->sql_freeresult($result);
+			if ($row) {
+				return $row;
+			}
+			
+			if(!function_exists('user_add'))
+			{
+				include($this->phpbb_root_path . 'includes/functions_user.' . $this->php_ext);
+			}
+
+			user_add($this->newUserRow($irow["nickname"], $irow["email"], $irow["password"]));
+			$result = $this->db->sql_query($sql);
+			$row = $this->db->sql_fetchrow($result);
+			$this->db->sql_freeresult($result);
+			if ($row) {
+				return $row;
+			}			
+		}
+		return array();
+	}
+	
+	private function newUserRow($username, $email, $password)
+	{
+		// first retrieve default group id
+		$sql = sprintf('SELECT group_id FROM %1$s WHERE group_name = \'%2$s\' AND group_type = \'%3$s\'', GROUPS_TABLE, $this->db->sql_escape('REGISTERED'), GROUP_SPECIAL);
+		$result = $this->db->sql_query($sql);
+		$row = $this->db->sql_fetchrow($result);
+		$this->db->sql_freeresult($result);
+
+		if(!$row)
+		{
+			trigger_error('NO_GROUP');
+		}
+
+		// generate user account data
+		return array(
+			'username'		=> $username,
+			'user_password'	=> '',
+			'user_email'	=> $email,
+			'group_id'		=> (int)$row['group_id'],
+			'user_type'		=> USER_NORMAL,			
+		);
+	}
 }
 ?>

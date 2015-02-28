4d0b45460bb5b0ee5db495c48ef176f09b482e00
Arseniy Krasnov <arseniy@krasnoff.org>
2015-02-28 19:36:56 +0300

    Init

---
+++ ext.php
@@ -0,0 +1,15 @@
+<?php
+/**
+*
+* @package phpBB Extension - Auth Instant
+* @copyright (c) 2015 Arseniy Krasnov
+* @license GPLv2 http://www.gnu.org/licenses/gpl-2.0.html
+*
+*/
+namespace arseniyk\instant;
+/**
+* @ignore
+*/
+class ext extends \phpbb\extension\base
+{
+}
